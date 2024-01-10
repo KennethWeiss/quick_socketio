89ad0315c379986eb79e0657610bf41e89735b8c
Kenny Weiss <kenny1weiss@aol.com>
2024-01-10 10:52:55 -0800

    Init start index.html

---
+++ app.py
@@ -0,0 +1,23 @@
+import socketio
+
+# create instance of socketio that you just imported
+#there are two types of server this standard and async which we will learn later
+sio = socketio.Server()
+#works with events handlers
+#ex. when client connects/disconnects
+
+app = socketio.WSGIApp(sio)
+# used to connect to wsgi webserver
+
+
+# couple ways to define events handlers
+# most convenitent is event decorator
+@sio.event
+def connect(sid, environ):
+    print(sid, 'connected')
+
+@sio.event
+def disconnect(sid, environ):
+    print(sid, 'disconnected')
+# need to take socketio application and transfer to wsgi app that can interact with something like gunicorn
+#socketio has wsgiapp create instance above
